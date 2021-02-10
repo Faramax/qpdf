934dab27443a3883784e7fbeec1fb3523c5b64a4
Mikhail Rodkin <mikhail.rodkin@faramax.com>
2021-02-10 22:13:27 +0300

    combine input from real and virtual keyboards

--- qpdflib/pdfview/virtual-keyboard.js
+++ qpdflib/pdfview/virtual-keyboard.js
@@ -1,13 +1,22 @@
 let Keyboard = window.SimpleKeyboard.default;
 
 let keyboard = new Keyboard({
-  onChange: input => onChange(input),
+  onKeyReleased: input => onKeyReleased(input),
   onKeyPress: button => onKeyPress(button)
 });
 
-function onChange(input){
-  document.querySelector(".toolbarField").value = input;
-  console.log("Input changed", input);
+function onKeyReleased(input){
+  switch (input) {
+  case "{shift}" :
+     break
+  case "{bksp}" :
+     document.querySelector(".toolbarField").value = document.querySelector(".toolbarField").value.slice(0,-1);
+     console.log("Bksp pressed", input);
+     break
+  default :
+     document.querySelector(".toolbarField").value += input;
+     console.log("Input changed", input);
+  }
 }
 
 function onKeyPress(button){
