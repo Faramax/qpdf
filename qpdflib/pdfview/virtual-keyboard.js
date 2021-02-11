e1b37775a8d435971c1b1c53fcf2a4a2fb43df9c
betulum <mikhail.rodkin@faramax.com>
2021-02-11 18:21:19 +0300

    virtual keyboar layouts: russian/english

--- qpdflib/pdfview/virtual-keyboard.js
+++ qpdflib/pdfview/virtual-keyboard.js
@@ -1,8 +1,14 @@
 let Keyboard = window.SimpleKeyboard.default;
+let KeyboardLayouts = window.SimpleKeyboardLayouts.default;
+
+let RussianLayout = {name:"russian", impl: new KeyboardLayouts().get("russian")};
+let EnglishLayout = {name:"english", impl: new KeyboardLayouts().get("english")};
+let layout = RussianLayout;
 
 let keyboard = new Keyboard({
   onChange: input => onChange(input),
-  onKeyPress: button => onKeyPress(button)
+  onKeyPress: button => onKeyPress(button),
+  layout: layout.impl
 });
 
 // Update simple-keyboard when input is changed directly
@@ -19,16 +25,31 @@ function onChange(input) {
 }
 
 function onKeyPress(button){
-  if (button === "{shift}" || button === "{lock}")
+  switch (button) {
+  case "{shift}":
     handleShift();
+    break
+  case "{Rus/Eng}":
+    languageSwitch();
+  }
   console.log("Button pressed", button);
 }
 
 function handleShift() {
   let currentLayout = keyboard.options.layoutName;
   let shiftToggle = currentLayout === "default" ? "shift" : "default";
+  keyboard.setOptions({layoutName: shiftToggle});
+}
 
-  keyboard.setOptions({
-    layoutName: shiftToggle
-  });
+function languageSwitch() {
+    switch (layout.name) {
+    case "russian":
+        layout = EnglishLayout
+        break;
+    case "english":
+        layout = RussianLayout
+        break;
+    default:
+    }
+    keyboard.setOptions({layout: layout.impl });
 }
